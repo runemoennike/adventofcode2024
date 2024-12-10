d8d5a190791ab7950a3caa2ceee08fba85d2e2a2
Rune A. Juel Mønnike <rajm@force.dk>
2024-12-10 22:20:48 +0100

    Day 6.

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -148,6 +148,8 @@ let input = myInput
 
 type Guard = { Pos: int*int; Dir: int*int }
 
+// Part 1
+
 let toMap (str:string) = 
     str.Trim().Split(System.Environment.NewLine)
     |> Array.map(fun s -> s |> Seq.map(fun c->
@@ -185,7 +187,7 @@ let rotateToNext dir =
 
 let takeSteps (map:int[,]) (initialGuard:Guard) = seq {
     let mutable guard = initialGuard
-    yield guard.Pos
+    yield guard
 
     let mutable prevPos = guard.Pos
     while (guard.Pos |> isInBoundsOf map) do
@@ -199,16 +201,58 @@ let takeSteps (map:int[,]) (initialGuard:Guard) = seq {
                 Pos = guard.Pos;
                 Dir = guard.Dir |> rotateToNext
               }
-        if guard.Pos <> prevPos && (guard.Pos |> isInBoundsOf map) then yield guard.Pos
+        if guard.Pos <> prevPos 
+            && (guard.Pos |> isInBoundsOf map) 
+            then yield guard
         prevPos <- guard.Pos
 }
 
 let map = input |> toMap
 let guard = input |> findGuard |> fun p -> { Pos = p; Dir = (0, -1) }
 let steps = guard |> takeSteps map |> List.ofSeq
-let distinctStepsCount = steps |> List.distinct |> List.length
+let distinctStepsCount = steps |> List.map _.Pos |> List.distinct |> List.length
 
-printfn $"%A{map}"
-printfn $"%A{steps}"
+//printfn $"%A{map}"
+//printfn $"%A{steps}"
 printfn $"{distinctStepsCount} {steps.Length}"
+
+
+// Part 2
+
+let doesLoop (map:int[,]) (initialGuard:Guard) = 
+    let mutable history = []
+
+    initialGuard
+    |> takeSteps map
+    |> Seq.exists(fun g ->
+        let result = history |> List.contains g
+        history <- history @ [g]
+        result
+    )
+
+let copyWithBarrierAt (guard:Guard) (map:int[,]) =
+    let map' = map |> Array2D.copy
+    let x, y = guard.Pos
+    map'[y, x] <- 1
+    map'
+
+let indexedSteps = 
+    steps
+    |> List.mapi(fun i g -> g, i)
+
+let blockerPositions =
+    indexedSteps 
+    |> List.filter(fun (g1, i1) ->
+        printfn $"checking {g1}"
+        let map' = map |> copyWithBarrierAt g1
+        guard |> doesLoop map'
+    )
+    |> List.map(fun t -> (fst t).Pos)
+    |> List.except [guard.Pos]
+    |> List.distinct
+
+//blockerPositions
+//    |> List.iter(fun g -> printfn $"%A{g}")
+
+printfn $"{blockerPositions.Length}"
 
